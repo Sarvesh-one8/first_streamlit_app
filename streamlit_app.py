36c54b2b2fd4f3442f91e5b9cb4692d1be0b25ab
Sarvesh Kavathekar <108080819+Sarvesh-one8@users.noreply.github.com>
2022-06-24 16:05:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My paraents new healthy dinner.')
 
@@ -6,3 +7,7 @@ streamlit.header('Breatfast Menu')
 streamlit.text('Oatmeal')
 streamlit.text('Kale')
 streamlit.text('Egg')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+
+streamlit.dataframe(my_fruit_list)
