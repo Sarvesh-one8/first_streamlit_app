40cee59fefdc08ffb77187740c22cfa93b38b5ca
Sarvesh Kavathekar <108080819+Sarvesh-one8@users.noreply.github.com>
2022-06-24 16:07:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ streamlit.text('Oatmeal')
 streamlit.text('Kale')
 streamlit.text('Egg')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.dataframe(my_fruit_list)
