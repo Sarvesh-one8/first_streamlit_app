e3b66f3a41a1aa99bc78a9593c37fe8dd0974aa4
Sarvesh Kavathekar <108080819+Sarvesh-one8@users.noreply.github.com>
2022-06-24 16:11:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,12 @@ import pandas
 
 streamlit.title('My paraents new healthy dinner.')
 
-streamlit.header('Breatfast Menu')
-streamlit.text('Oatmeal')
-streamlit.text('Kale')
-streamlit.text('Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
