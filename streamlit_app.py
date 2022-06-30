1686139b216caef1a965401455c0b2d7f7e3a8b8
Sarvesh Kavathekar <108080819+Sarvesh-one8@users.noreply.github.com>
2022-06-30 16:23:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,19 +44,6 @@ try:
 
 except URLError as e:
   streamlit.error()
-streamlit.stop()
-
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
-# streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-#streamlit.dataframe(fruityvice_normalized)
-
-#streamlit.header("Fruityvice Fruit Advice!")
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
