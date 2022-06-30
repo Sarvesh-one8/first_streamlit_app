a34fc7caded57b61649102e80b5f6a233587b196
Sarvesh Kavathekar <108080819+Sarvesh-one8@users.noreply.github.com>
2022-06-30 12:14:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+snowflake-connector-python
 import snowflake.connector
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # disply the api
