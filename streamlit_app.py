7bbc87634aa8d12a20f2f56fd1aed43251986e2d
Sarvesh Kavathekar <108080819+Sarvesh-one8@users.noreply.github.com>
2022-06-30 16:09:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,17 +38,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 #streamlit.dataframe(fruityvice_normalized)
 
 #streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-      streamlit.error("PLEASE ENTER")
-      else:
-           fruityvice_response = requests.get("https://fruityvice.com/api/fruit"+ fruit_choice)
-           fruityvice_normalized =pands.json_normalize(fruityvice_response.json())
-            streamlit.dataframe(fruityvice_normalized)
- except URLERROR as e:
-  streamlit.error()
-streamlit.stop()
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
